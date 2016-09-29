3b0d0d1501e65a1b7afbf1e180f0f7ce6e623ff7
Sebastian Hardt <s.hardt@micromata.de>
2016-09-29 14:08:08 +0200

    Brightness added

--- Readme.md
+++ Readme.md
@@ -6,4 +6,5 @@
 
 ### Testing
 
-curl -H "Authorization: cf97edf7-5ff9-4489-9ba0-8f89d4ee8144" -F imageData=@tux.png http://localhost:3000
+curl -H "Authorization: cf97edf7-5ff9-4489-9ba0-8f89d4ee8144" -F imageData=@tux.png http://localhost:3000/image
+curl -H "Authorization: cf97edf7-5ff9-4489-9ba0-8f89d4ee8144" http://localhost:3000/brightness/255
--- backend/server.js
+++ backend/server.js
@@ -1,91 +1,137 @@
 /**
-* This tiny application reads the data from a post file and turns them into pixels rgb values.
-* Those are send over the mqtt bus.
-*/
+ * This tiny application reads the data from a post file and turns them into pixels rgb values.
+ * Those are send over the mqtt bus.
+ */
 
 
 /**
-* Load configuration
-*/
+ * Load configuration
+ */
 var config = require("./config.json")
 
 /**
-* Logging is nice
-*/
+ * Logging is nice
+ */
 var winston = require('winston');
 
 
 /**
-* We need some mqtt stuff to send the data to the display
-*/
+ * We need some mqtt stuff to send the data to the display
+ */
 var mqtt = require('mqtt');
 var mqttClient = mqtt.connect(config.mqtt.server);
-mqttClient.on('connect', function () {
+mqttClient.on('connect', function() {
   var date = new Date();
-  //mqttClient.subscribe(config.mqtt.toppic);
-  winston.info('[MQTT] : Connected to server: '+config.mqtt.server+' on toppic: '+config.mqtt.topic);
-  mqttClient.publish(config.mqtt.topic, 'PngToPixelServerConnected: '+date);
+  winston.info('[MQTT] : Connected to server: ' + config.mqtt.server + ' on toppic: ' + config.mqtt.topicValue);
+  mqttClient.publish(config.mqtt.topicStatus, 'PngToPixelServerConnected: ' + date);
 });
 
 /**
-* Libary for reading pixel data
-*/
+ * Libary for reading pixel data
+ */
 var getPixels = require("get-pixels")
 
 /**
-* Webserver stuff goes here
-*/
+ * Webserver stuff goes here
+ */
 var express = require('express');
 var app = express();
 
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var multer = require('multer')
+var upload = multer({
+  dest: 'uploads/'
+})
 
 var fs = require('fs');
 
-app.post('/', upload.single('imageData'), function (req, res, next) {
+app.post('/image', upload.single('imageData'), function(req, res, next) {
 
   // check if we have the token in the header
-  winston.info('[WEB] : Checking if sended token matches configured token');
-  if(req.headers.authorization === undefined) {
-    var message =  'No token was send as header.';
-    winston.error('[WEB] : '+message);
-    res.status(401).send(message);
+  if (checkToken(req, res) == false) {
     return;
+  }
+
+  winston.info('[WEB] : Uploaded file found:', req.file);
+  readImageData(req.file.path, res);
+
+});
+
+app.get('/brightness/:value', function(req, res) {
+  // check if we have the token in the header
+  if (checkToken(req, res) == false) {
+    return;
+  }
+  brightness = req.params.value;
+
+  // check if it is defined
+  if (brightness === undefined) {
+    sendError(res, 'No brighntess given', 500);
+    return;
+  }
+
+  // check if it is a number
+  if (isNaN(brightness) == true) {
+    sendError(res, 'Brightness is not a number: ' + brightness, 500);
+    return;
+  }
+
+  // check if it is in the rane
+  var brighntessNr = Number(brightness);
+  if (brighntessNr > 255 || brighntessNr < 0) {
+    sendError(res, 'Brightness ' + brightness + ' is not in range 0..255', 500);
+    return;
+  }
+
+  mqttClient.publish(config.mqtt.topicBrightness, brightness, function(error, granted) {
+    res.send('Send brightness: ' + brightness + ' to mqtt !');
+  });
+
+});
+
+app.listen(config.webServerPort, function() {
+  winston.info('[WEB] : Webapp listening on port ' + config.webServerPort + '!');
+});
+
+var checkToken = function(req, res) {
+  // check if we have the token in the header
+  winston.info('[WEB] : Checking if sended token matches configured token');
+  if (req.headers.authorization === undefined) {
+    sendError(res, message, 401);
+    return false;
   }
 
   // check if the token is correct
-  if(req.headers.authorization !== config.token) {
-    var message = 'The sended token: '+req.headers.authorization+' does not equals: '+config.token;
-    winston.error('[WEB] : '+message);
-    res.status(401).send('Token is not correct.');
-    return;
+  if (req.headers.authorization !== config.token) {
+    var message = 'The sended token: ' + req.headers.authorization + ' does not equals: ' + config.token;
+    sendError(res, message, 401);
+    return false;
   }
 
-  winston.info('[WEB] : Uploaded file found:',req.file);
-
-  readImageData(req.file.path,res);
-
-});
-
-app.listen(config.webServerPort, function () {
-  winston.info('[WEB] : Webapp listening on port '+config.webServerPort+'!');
-});
+  return true;
+}
 
 /**
-* Reads the daata from the image and turns it into the data which is send over the mqtt bus
-*/
+ * Sends an error to the client
+ */
+var sendError = function(res, message, status) {
+  winston.error('[WEB] : ' + message);
+  res.status(status).send(message);
+}
+
+/**
+ * Reads the daata from the image and turns it into the data which is send over the mqtt bus
+ */
 var readImageData = function(imagePath, res) {
 
   // we need a png at the end or getpixels will die :)
-  var newImgPath = imagePath+'.png';
-  fs.renameSync(imagePath,newImgPath);
+  var newImgPath = imagePath + '.png';
+  fs.renameSync(imagePath, newImgPath);
 
   getPixels(newImgPath, function(err, pixels) {
-    if(err) {
-      var message = 'Could not read pixel data from: '+newImgPath;
+    if (err) {
+      var message = 'Could not read pixel data from: ' + newImgPath;
       fs.unlinkSync(newImgPath);
-      winston.error('[IMG] : '+message,err)
+      winston.error('[IMG] : ' + message, err)
       res.status(500).send(message);
       return
     }
@@ -95,50 +141,50 @@ var readImageData = function(imagePath, res) {
 
     var data = parseImageData(pixels);
 
-    sendDataOverMqttBus(data,res);
+    sendDataOverMqttBus(data, res);
   })
 };
 
 /**
-* Sends the data over mqtt bus and aknowledges it to the caller
-*/
-var sendDataOverMqttBus = function(data,res) {
-  mqttClient.publish(config.mqtt.topic, data,function(error,granted) {
+ * Sends the data over mqtt bus and aknowledges it to the caller
+ */
+var sendDataOverMqttBus = function(data, res) {
+  mqttClient.publish(config.mqtt.topicValue, data, function(error, granted) {
     res.send('Send message to mqtt !');
   });
 };
 
 /**
-* Parses the data from the pixels informations
-*/
+ * Parses the data from the pixels informations
+ */
 var parseImageData = function(pixels) {
   var res = '';
   var sep = '';
   var dataPos = 0;
-  for(y=0; y < pixels.shape[1]; y++) {
-    for(x=0; x < pixels.shape[0]; x++) {
+  for (y = 0; y < pixels.shape[1]; y++) {
+    for (x = 0; x < pixels.shape[0]; x++) {
       // read the rgb information
       var rgbData = '';
-      for(i=0; i < 3; i++) {
+      for (i = 0; i < 3; i++) {
         // convert it to hex
-        rgbData+=dec2hex(pixels.data[dataPos]);
+        rgbData += dec2hex(pixels.data[dataPos]);
         dataPos++;
       }
 
-      res+=sep+rgbData;
+      res += sep + rgbData;
       sep = ',';
 
       // there might be some more informations we just skip
-      dataPos+=(pixels.shape[2]-3);
+      dataPos += (pixels.shape[2] - 3);
     }
   }
-  winston.debug('[IMG] : data: '+res);
+  winston.debug('[IMG] : data: ' + res);
   return res;
 }
 
 /**
-* Converts number to hex and adds padding
-*/
+ * Converts number to hex and adds padding
+ */
 function dec2hex(d) {
-    return  ('0'+(Number(d).toString(16))).slice(-2).toUpperCase();
+  return ('0' + (Number(d).toString(16))).slice(-2).toUpperCase();
 }
